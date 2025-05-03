f26b118f2ce394bf2d63d0ac27e044ce8762d21c
Arihant <34059418+Arihantawasthi@users.noreply.github.com>
2025-05-04 02:06:52 +0530

    feat: created packet and packet encoder

--- cmd/sagectl/main.go
+++ cmd/sagectl/main.go
@@ -8,10 +8,32 @@ import (
 )
 
 func main() {
+    command := os.Args[1]
+    var cmdType byte
+    if command == "list" {
+        cmdType = spmp.TypeList
+    }
+
+    serviceName := ""
+    if len(os.Args) > 1 {
+        serviceName = os.Args[2]
+    }
+
     client := spmp.NewSPMPClient()
-    data, err := client.Show()
+    packet, err := spmp.NewPacket(spmp.V1, spmp.TEXTEncoding, cmdType, []byte(serviceName))
+    if err != nil {
+        fmt.Fprintf(os.Stderr, "error while creating packet: %s", err)
+    }
+
+    packetBytes, err := packet.Encode()
+    if err != nil {
+        fmt.Fprintf(os.Stderr, "error while encoding packet: %s", err)
+    }
+
+    data, err := client.SendPacket(packetBytes)
     if err != nil {
         fmt.Fprintf(os.Stderr, "error while fetching info: %s", err)
     }
+
     fmt.Println(data)
 }
--- internal/spmp/client.go
+++ internal/spmp/client.go
@@ -6,31 +6,31 @@ import (
 )
 
 type SPMPClient struct {
-    socketPath string
+	socketPath string
 }
 
 func NewSPMPClient() *SPMPClient {
-    return &SPMPClient{
-        socketPath: "/tmp/sage.sock",
-    }
+	return &SPMPClient{
+		socketPath: "/tmp/sage.sock",
+	}
 }
 
-func (c *SPMPClient) Show() (string, error) {
-    conn, err := net.Dial("unix", c.socketPath)
-    if err != nil {
-        return "", fmt.Errorf("error connecting to unix socket: %w", err)
-    }
-    defer conn.Close()
-    message := []byte("hello")
-    _, err = conn.Write(message)
-    if err != nil {
-        return "", fmt.Errorf("error writing to the connection: %v\n", err)
-    }
+func (c *SPMPClient) SendPacket(pkt []byte) (string, error) {
+	conn, err := net.Dial("unix", c.socketPath)
+	if err != nil {
+		return "", fmt.Errorf("error connecting to unix socket: %w", err)
+	}
+	defer conn.Close()
+
+	_, err = conn.Write(pkt)
+	if err != nil {
+		return "", fmt.Errorf("error writing to the connection: %v\n", err)
+	}
 
 	buffer := make([]byte, 1024)
 	n, err := conn.Read(buffer)
 	if err != nil {
 		return "", fmt.Errorf("error reading from the connection: %v\n", err)
 	}
-    return string(buffer[:n]), nil
+	return string(buffer[:n]), nil
 }
